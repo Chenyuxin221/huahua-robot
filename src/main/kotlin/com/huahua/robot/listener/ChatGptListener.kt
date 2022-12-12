7089f392326b137100fcba0a7e1b706d0af68bae
Chenyuxin221 <shuimoxi@vip.qq.com>
2022-12-12 21:50:34 +0800

    优化代码 修复了缓存和数据库进行同步时的错误 修复了chatGtp无法使用的错误 新增了chatGtp登录的相关代码 优化了部分文字提示

---
+++ src/main/kotlin/com/huahua/robot/listener/ChatGptListener.kt
@@ -0,0 +1,145 @@
+package com.huahua.robot.listener
+
+import com.alibaba.fastjson2.JSON
+import com.alibaba.fastjson2.JSONException
+import com.github.plexpt.chatgpt.Chatbot
+import com.huahua.robot.core.annotation.RobotListen
+import com.huahua.robot.core.common.RobotCore
+import com.huahua.robot.core.common.logger
+import com.huahua.robot.core.common.send
+import com.huahua.robot.service.SwitchSateService
+import com.huahua.robot.utils.Permission
+import com.huahua.robot.utils.PermissionUtil.Companion.authorPermission
+import love.forte.simboot.annotation.Filter
+import love.forte.simboot.annotation.FilterValue
+import love.forte.simboot.filter.MatchType
+import love.forte.simbot.ID
+import love.forte.simbot.event.FriendMessageEvent
+import love.forte.simbot.event.GroupMessageEvent
+import love.forte.simbot.event.MessageEvent
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.logging.LogLevel
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.RedisSerializer
+import org.springframework.stereotype.Component
+import javax.annotation.Resource
+
+/**
+ * ClassName: ChatGptListener
+ * @description
+ * @author 花云端
+ * @date 2022-12-12 21:15
+ */
+@Component
+class ChatGptListener(
+    val switchSateService: SwitchSateService,
+) {
+    /**
+     * 获取配置项里面的token和cfClearance
+     */
+    @Value("\${huahua.config.gpt.token:#{null}}")
+    var chatGtpToken: String? = ""
+
+    @Value("\${huahua.config.gpt.cf_clearance:#{null}}")
+    var cfClearance: String? = ""
+
+    @Resource
+    lateinit var redisTemplate: RedisTemplate<String, String>
+
+    @RobotListen("ChatGpt", isBoot = true)
+    @Filter(">{{questions}}", matchType = MatchType.REGEX_MATCHES)
+    suspend fun MessageEvent.chatGpt(@FilterValue("questions") questions: String) {
+        try {
+            if (this is GroupMessageEvent) {
+                val result = switchSateService.get(group().id.toString(), "聊天")
+                if (result == null) {  //没有将群加入缓存
+                    switchSateService.set(group().id.toString(), "聊天", false)
+                    return
+                }
+                if (!result) {   //处于关闭状态
+                    logger { "未开启" }
+                    return
+                }
+            }
+            redisTemplate.valueSerializer = RedisSerializer.json()
+            var tokenTemplate = redisTemplate.opsForValue().get("chatGtpToken") //读取缓存里的token
+            var cfClearanceTemplate = redisTemplate.opsForValue().get("chatGtpCfClearance") //读取缓存里的cfClearance
+            if (tokenTemplate.isNullOrEmpty() || cfClearanceTemplate.isNullOrEmpty()) {   //没有token或cfClearance缓存
+                if (chatGtpToken == null) {    //没有配置token
+                    logger(LogLevel.ERROR) {
+                        "如需要此功能，请配置token或者使用指令（token绑定+Token）"
+                    }
+                    return
+                }
+                if (cfClearance == null) { //没有配置cfClearance
+                    logger(LogLevel.ERROR) {
+                        "如需要此功能，请配置cfClearance或者使用指令（cf绑定+cfClearance）"
+                    }
+                    return
+                }
+                redisTemplate.opsForValue().set("chatGtpToken", chatGtpToken!!) //将token缓存到redis
+                redisTemplate.opsForValue().set("chatGtpCfClearance", cfClearance!!)
+            }
+            tokenTemplate = redisTemplate.opsForValue().get("chatGtpToken") // 重新读取缓存内的token
+            cfClearanceTemplate = redisTemplate.opsForValue().get("chatGtpCfClearance")
+            val userAgent =
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46"
+            val response = Chatbot(tokenTemplate, cfClearanceTemplate, userAgent).getChatResponse(questions)
+            val message = response["message"].toString()
+            if (message.isEmpty()) {
+                logger { JSON.toJSONString(response) }
+                return
+            }
+            reply(message)
+        } catch (e: JSONException) {
+            reply("哎呀，出错啦")
+            e.printStackTrace()
+        } catch (e: Exception) {
+            reply(e.message ?: "出错了")
+            e.printStackTrace()
+        }
+    }
+
+    @RobotListen("token绑定", isBoot = true)
+    @Filter("token绑定{{token}}", matchType = MatchType.REGEX_MATCHES)
+    suspend fun MessageEvent.bindingToken(@FilterValue("token") token: String) {
+        val permission = authorOwnOrNotPermission(this)
+        if (!permission) {
+            send("哎呀，你的权限不足,无法绑定你的token呢")
+            return
+        }
+
+        redisTemplate.opsForValue().set("chatGtpToken", token) // 将接收到的token缓存进redis
+        val result = redisTemplate.opsForValue().get("chatGtpToken")
+        if (result.isNullOrEmpty()) {
+            reply("qaq绑定失败了~")
+            return
+        }
+        reply("绑定成功,你现在的Token为：$result")
+
+    }
+
+    @RobotListen("cfClearance绑定", isBoot = true)
+    @Filter("cf绑定{{cfClearance}}", matchType = MatchType.REGEX_MATCHES)
+    suspend fun MessageEvent.bindingCfClearance(@FilterValue("cfClearance") cfClearance: String) {
+        val permission: Boolean = authorOwnOrNotPermission(this)
+        if (!permission) {
+            send("哎呀，你的权限不足,无法绑定你的cfClearance呢")
+            return
+        }
+
+        redisTemplate.opsForValue().set("chatGtpCfClearance", cfClearance) // 将接收到的token缓存进redis
+        val result = redisTemplate.opsForValue().get("chatGtpCfClearance")
+        if (result.isNullOrEmpty()) {
+            reply("qaq绑定失败了~")
+            return
+        }
+        reply("绑定成功,你现在的chatGtpCfClearance为：$result")
+    }
+
+    private suspend fun authorOwnOrNotPermission(event: MessageEvent) = when (event) {
+        is GroupMessageEvent -> event.author().id == RobotCore.ADMINISTRATOR.ID || event.authorPermission() != Permission.MEMBER
+        is FriendMessageEvent -> event.friend().id == RobotCore.ADMINISTRATOR.ID
+        else -> false
+    }
+}
